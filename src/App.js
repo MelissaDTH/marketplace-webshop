39b0b47322963385deb91139ef169602b744e7a5
MelissaDTH <melissa.hart@ziggo.nl>
2020-01-06 15:45:55 +0100

    added navbar with links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Navbar, NavDropdown, Nav, Form, FormControl, Button } from "react-bootstrap";
 import "./App.css";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Fragment>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="/home">Marketplace</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <NavDropdown
+              title="Product categories"
+              id="collasible-nav-dropdown"
+            >
+              <NavDropdown.Item href="/products/1">
+                Electronics
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/products/2">Kitchen</NavDropdown.Item>
+              <NavDropdown.Item href="/products/3">Clothing</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="/login">Log in</Nav.Link>
+            <Nav.Link eventKey={2} href="/signup">
+              Sign up
+            </Nav.Link>
+            <Button href="/cart">Shopping Cart</Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Fragment>
+  );
 }
 
 export default App;
