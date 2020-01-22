3c2aeb1ead1fc7063c264be0078ffb36135093cb
MelissaDTH <melissa.hart@ziggo.nl>
2020-01-22 18:18:49 +0100

    fraud risk is shown at details page

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -2,10 +2,11 @@ import React from "react";
 import "./ProductDetails.css";
 import { Button } from "react-bootstrap";
 import { connect } from "react-redux";
-import { addProduct } from '../../actions/cart'
+import { addProduct } from "../../actions/cart";
 
 function ProductDetails(props) {
   const product = props.product;
+  console.log("product", product);
 
   return (
     <div className="details">
@@ -13,30 +14,40 @@ function ProductDetails(props) {
         <div className="details_img-container">
           <img src={product.picture} height={250} alt="product" />
         </div>
-        <div className="details__info">
+        <div className="details_info">
           <h1 className="details-title">
             <b>{product.name}</b>
           </h1>
           <h3>
             <b>€ {product.price}</b>{" "}
           </h3>
-          <p>
-            <b>Color:</b> {product.color}
-          </p>
-          <p>
-            {/* <b>Seller:</b> {props.product.user.username} */}
+          <p
+            className={
+              product.risk > 70
+                ? "details-risk red"
+                : product.risk > 20
+                ? "details-risk yellow"
+                : "details-risk green"
+            }
+          >
+            <b>Fraud Risk: {product.risk}%</b> 
           </p>
           <Button
             variant="warning"
             className="add-to-cart"
             onClick={() => props.addProduct(product)}
-          >
-            Add to Cart
+          > Add to Cart
           </Button>
         </div>
-        <br />
+        
         <div className="product-descript">
           {" "}
+          <p>
+            <b>Seller:</b> {product.user.username}
+            <br />
+            <b>Color:</b> {product.color}
+            <br />
+            </p>
           <p>
             <b>Description by seller: </b>
             <br />
@@ -48,5 +59,4 @@ function ProductDetails(props) {
   );
 }
 
-export default connect(
-	null, { addProduct })(ProductDetails);
+export default connect(null, { addProduct })(ProductDetails);
